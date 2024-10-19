df3c23b56d363773a6f4d8cbaa6137c019b626f7
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-20 01:54:28 +0500

    feat: add update user

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -52,3 +52,41 @@ export function createUser(req: http.IncomingMessage, res: http.ServerResponse):
     }
   });
 }
+
+export function updateUser(
+  req: http.IncomingMessage,
+  res: http.ServerResponse,
+  userId: string | undefined,
+): void {
+  if (userId && uuidValidateV4(userId)) {
+    const user = store.get(userId as string);
+
+    if (user) {
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+        try {
+          const newUserData = JSON.parse(body);
+          const updatedData = { ...user, ...newUserData };
+
+          store.set(userId, updatedData);
+          res.writeHead(201, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(updatedData));
+        } catch {
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ message: 'Error parsing JSON' }));
+        }
+      });
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: `User with ${userId} does not exist` }));
+    }
+  } else {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User Id is invalid' }));
+  }
+}
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import http from 'http';
-import { createUser, getUserById, getUsers } from '../controllers/usersController';
+import { createUser, getUserById, getUsers, updateUser } from '../controllers/usersController';
 
 export function handleUserRequest(req: http.IncomingMessage, res: http.ServerResponse): void {
   const method = req.method;
@@ -12,6 +12,9 @@ export function handleUserRequest(req: http.IncomingMessage, res: http.ServerRes
   } else if (url?.startsWith('/users/') && method === 'GET') {
     const id = url.split('/')[2];
     getUserById(res, id);
+  } else if (url?.startsWith('/users/') && method === 'PUT') {
+    const id = url.split('/')[2];
+    updateUser(req, res, id);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
