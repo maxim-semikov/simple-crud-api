e0650c7148e4d416bc23a8018ce6de229688c863
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-20 15:26:27 +0500

    feat: add test covered GET /api/users should return array of users

--- src/controllers/usersController/usersController.test.ts
+++ src/controllers/usersController/usersController.test.ts
@@ -1,18 +1,30 @@
 import supertest from 'supertest';
 import { SimpleCRUDServer } from '../../server';
+import { store, User } from '../../store';
+import { createUUID } from '../../helpers';
 
 describe('Simple CRUD API', () => {
   let server: SimpleCRUDServer;
   const mockStore = new Map();
+  let id: string;
+  let testUser: User = {} as User;
 
   beforeAll(() => {
     server = new SimpleCRUDServer(mockStore);
     server.start('3010');
+    id = createUUID();
+    testUser = {
+      id,
+      username: ' John Smith',
+      age: 30,
+      hobbies: ['coding'],
+    };
   });
 
   afterAll(() => {
     server.stop();
     mockStore.clear();
+    testUser = {} as User;
   });
 
   it('GET /api/users should return empty array of users', async () => {
@@ -20,6 +32,14 @@ describe('Simple CRUD API', () => {
 
     expect(statusCode).toEqual(200);
     expect(text).toEqual('[]');
+  });
+
+  it('GET /api/users should return array of users', async () => {
+    store.set(id, testUser);
+    const { statusCode, text } = await supertest(server['server']).get(`/api/users/${id}`);
+
+    expect(statusCode).toEqual(200);
+    expect(text).toEqual(JSON.stringify(testUser));
   });
 
   test('GET some-non/existing/resource should return 404', async () => {
